acf473f989484c631590ee92aec3ff7007b4fa56
T7SEN <a.hitelare2@gmail.com>
2025-12-14 08:16:49 +0200

    chore: verify husky setup

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,7 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+// Utility function to merge class names with Tailwind CSS support
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
