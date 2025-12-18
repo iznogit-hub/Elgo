ae58244dd06a4a72c316e563fb5cc87884de77bf
T7SEN <a.hitelare2@gmail.com>
2025-12-18 08:20:19 +0200

    fix(next.config): update Permissions-Policy to allow microphone access from self

--- next.config.ts
+++ next.config.ts
@@ -52,7 +52,7 @@ const nextConfig: NextConfig = {
           {
             key: "Permissions-Policy",
             value:
-              "camera=(), microphone=(), geolocation=(), browsing-topics=()",
+              "camera=(), microphone=(self), geolocation=(), browsing-topics=()",
           },
         ],
       },
